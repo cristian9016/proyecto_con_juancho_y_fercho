d9be9094bb5675e4b06122be876194d81749b973
Cristian Muñoz <cristian.munoz@rappi.com>
2021-02-18 20:38:03 -0500

    se borro codigo inutil

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,31 +1,12 @@
 package com.example.firstapplication
 
-import android.content.DialogInterface
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var x = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val dialog = AlertDialog.Builder(this).apply {
-            setMessage("Mensaje")
-            setTitle("Titulo")
-            setPositiveButton("",object:DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface?, which: Int) {
-                    
-                }
-
-            })
-        }
-
-
-
     }
-
 }
